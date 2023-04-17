583d249eb6cc3d499d4940e1c9c82540dbb3ef48
Christian Schou Oxvig <christian.oxvig@deic.dk>
2023-04-17 14:25:28 +0200

    Implemented python version mismatch errer early check AB#596

--- cotainr/__init__.py
+++ cotainr/__init__.py
@@ -7,6 +7,9 @@ Licensed under the European Union Public License (EUPL) 1.2
 
 """
 
+import platform
+import sys
+
 __version__ = "2023.02.0"
 _minimum_dependency_version = {
     # Versions must be specified as a (major, minor, patchlevel) tuple of
@@ -15,3 +18,14 @@ _minimum_dependency_version = {
     "apptainer": (1, 0, 0),
     "singularity": (3, 7, 4),
 }
+
+# Error early on too old Python version
+_py_ver = platform.python_version_tuple()
+if tuple(map(int, _py_ver)) < (3, 8, 0):
+    sys.exit(
+        (
+            "\033[91mCotainr requires Python>=3.8, you are running Python==%s from %s, "
+            "ABORTING!\033[0m"
+        )
+        % (".".join(_py_ver), sys.executable)
+    )
