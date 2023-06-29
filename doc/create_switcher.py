4fdd45ea288d64348aec7dc2d56e1501cd9ced98
René Løwe Jacobsen <rlj@rloewe.net>
2023-06-29 13:28:25 +0200

    Update doc/create_switcher.py
    Co-authored-by: Christian Schou Oxvig <5367057+Chroxvi@users.noreply.github.com>

--- doc/create_switcher.py
+++ doc/create_switcher.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import subprocess
 import sys
 
-sys.path.insert(0, f'{Path("..").resolve()}')
+sys.path.insert(0, f'{(Path(__file__) / "..").resolve()}')
 
 import cotainr
 
