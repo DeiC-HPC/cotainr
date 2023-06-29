bdc3a70abd3c57ead738c85ab46f9932ad9d24cb
René Løwe Jacobsen <rlj@rloewe.net>
2023-06-29 13:36:06 +0200

    Add regex and link to documentation in release notes

--- doc/create_switcher.py
+++ doc/create_switcher.py
@@ -1,9 +1,23 @@
+"""
+cotainr - a user space Apptainer/Singularity container builder.
+
+Copyright DeiC, deic.dk
+Licensed under the European Union Public License (EUPL) 1.2
+- see the LICENSE file for details.
+
+Automatically create the version switcher configuration for the PyData Sphinx Theme.
+See https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/version-dropdown.html#add-a-json-file-to-define-your-switcher-s-versions for details.
+
+The switcher will contain the latest 4 releases. This should be run before making a tag for the latest version.
+More information about releasing can be found here: https://cotainr.readthedocs.io/en/latest/development/releasing.html
+"""
 import json
 from pathlib import Path
+from re import match
 import subprocess
 import sys
 
-sys.path.insert(0, f'{(Path(__file__) / "..").resolve()}')
+sys.path.insert(0, f'{(Path(__file__) / "../..").resolve()}')
 
 import cotainr
 
@@ -14,7 +28,7 @@ tags = [
             ["git", "--no-pager", "tag"], capture_output=True, text=True
         ).stdout.splitlines()
     )
-    if tag.startswith("20")
+    if match("^20[0-9]{2}\.(0[1-9]|10|11|12)\.[0-9]+$", tag) is not None
 ]
 tags.reverse()
 
@@ -41,5 +55,4 @@ for tag in tags[0:3]:
     )
 
 
-with open("_static/switcher.json", "w") as f:
-    json.dump(switcher, f)
+(Path(__file__) / "../_static/switcher.json").resolve().write_text(json.dumps(switcher))
