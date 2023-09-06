f0d6acc655108c318e9fdd6eb56b85142d8f8d30
Christian Schou Oxvig <christian.oxvig@deic.dk>
2023-09-06 13:38:21 +0200

    AB#733 Cotainr demo updates before LUMI user coffee break, Sep. 27, 2023

---
+++ demo/bash_snippets.sh
@@ -0,0 +1,3 @@
+# pygmentize -l console -f rtf -o bash_snippets.rtf  bash_snippets.sh
+$ conda env create --file my_conda_env.yml
+$ cotainr build my_container.sif --system lumi-g --conda-env my_conda_env.yml
