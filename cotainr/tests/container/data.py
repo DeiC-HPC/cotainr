b245c82fa3826517c31e782627067169fa0095a9
Aarni Koskela <akx@iki.fi>
2024-07-24 20:18:50 +0300

    DRY cotainr.tests.container.data

--- cotainr/tests/container/data.py
+++ cotainr/tests/container/data.py
@@ -7,50 +7,41 @@ Licensed under the European Union Public License (EUPL) 1.2
 
 """
 
+import pathlib
 import subprocess
 
 import pytest
 
 
-@pytest.fixture(scope="session")
-def data_cached_alpine_sif(tmp_path_factory):
-    """A session scope cached SIF image of the latest alpine linux container."""
-    singularity_image_path = (
-        tmp_path_factory.mktemp("singularity_images") / "alpine_latest.sif"
-    )
+def _pull_image(image_name: str, target_path: pathlib.Path) -> pathlib.Path:
     subprocess.run(
         args=[
             "singularity",
             "pull",
-            f"{singularity_image_path.resolve()}",
-            "docker://alpine:latest",
+            f"{target_path}",
+            f"docker://{image_name}",
         ],
         capture_output=True,
         check=True,
         text=True,
     )
-    assert singularity_image_path.exists()
+    assert target_path.exists()
+    return target_path
 
-    return singularity_image_path
+
+@pytest.fixture(scope="session")
+def data_cached_alpine_sif(tmp_path_factory):
+    """A session scope cached SIF image of the latest alpine linux container."""
+    return _pull_image(
+        "alpine:latest",
+        tmp_path_factory.mktemp("singularity_images") / "alpine_latest.sif",
+    )
 
 
 @pytest.fixture(scope="session")
 def data_cached_ubuntu_sif(tmp_path_factory):
     """A session scope cached SIF image of the latest ubuntu linux container."""
-    singularity_image_path = (
-        tmp_path_factory.mktemp("singularity_images") / "ubuntu_latest.sif"
+    return _pull_image(
+        "ubuntu:latest",
+        tmp_path_factory.mktemp("singularity_images") / "ubuntu_latest.sif",
     )
-    subprocess.run(
-        args=[
-            "singularity",
-            "pull",
-            f"{singularity_image_path.resolve()}",
-            "docker://ubuntu:latest",
-        ],
-        capture_output=True,
-        check=True,
-        text=True,
-    )
-    assert singularity_image_path.exists()
-
-    return singularity_image_path
