b426fa5b05c56cf9399b7411c750184e32333f83
Aarni Koskela <akx@iki.fi>
2024-09-11 08:55:37 +0300

    Enable Ruff D (docstrings) and rework docstrings as suitable

--- cotainr/pack.py
+++ cotainr/pack.py
@@ -392,9 +392,7 @@ class CondaInstall:
                 return True
 
         class NoEmptyLinesFilter(logging.Filter):
-            """
-            Remove any empty lines.
-            """
+            """Remove any empty lines."""
 
             def filter(self, record):
                 return record.msg.strip() != ""
