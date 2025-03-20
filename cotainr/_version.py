f88e071292b31ea3f966d4e1ef07a1e1d6c49ee2
Christian Schou Oxvig <christian.oxvig@deic.dk>
2025-03-20 11:27:26 +0100

    Final draft of cotainr._version incl. tests.

--- cotainr/_version.py
+++ cotainr/_version.py
@@ -1,13 +1,14 @@
 """
 cotainr - a user space Apptainer/Singularity container builder.
 
-Copyright DeiC, deic.dk
-Licensed under the European Union Public License (EUPL) 1.2
+Copyright DeiC, deic.dk Licensed under the European Union Public License (EUPL)
+1.2
  - see the LICENSE file for details.
 
 This module implements dynamic version number computation for cotainr based on
 git history (if in a git development environment) or the installed package
-version (if used from an installed cotainr package).
+version (if used from an installed cotainr package). If neither is available,
+the reported version is "<unknown version>".
 
 Adapted from the hatch-vcs-footgun-example, see
 <https://github.com/maresb/hatch-vcs-footgun-example/>.
@@ -17,19 +18,23 @@ Functions
 _get_hatch_version()
     Compute the version number in a development environment.
 _get_importlib_metadata_version()
-    Get the version number using importlib.metadata
+    Get the version number for an installed cotainr package.
 """
 
 from pathlib import Path
 
 
 def _get_hatch_version():
-    """Compute the version number in a development environment.
+    """
+    Compute the version number in a development environment.
+
+    Uses the hatchling package functionality to determine the version number.
 
     Returns
     -------
     str or None
-        The version number or `None` if Hatchling is not installed.
+        The version number or `None` if hatchling is unable to determine the
+        version number.
 
     """
 
@@ -41,11 +46,22 @@ def _get_hatch_version():
 
     cotainr_root_path = Path(__file__).parents[1]
     metadata = ProjectMetadata(root=cotainr_root_path, plugin_manager=PluginManager())
-    return metadata.hatch.version.cached
+
+    try:
+        vcs_version = metadata.hatch.version.cached
+    except Exception:
+        # If "cached" doesn't exist or raises an exception, give up and return None.
+        return None
+
+    return vcs_version
 
 
 def _get_importlib_metadata_version():
-    """Get the version number using importlib.metadata
+    """
+    Get the version number for an installed cotainr package.
+
+    Uses standard importlib.metadata functions to get the version number for an
+    installed package.
 
     Returns
     -------
@@ -57,9 +73,10 @@ def _get_importlib_metadata_version():
 
     try:
         package_version = version(__package__)
-        return package_version
     except PackageNotFoundError:
         return None
+
+    return package_version
 
 
 # The cotainr version based on git history or the installed package version. If
