1c5af8ff33ff5efc879720be08d5485c8055bf16
Christian Schou Oxvig <christian.oxvig@deic.dk>
2025-03-27 14:19:03 +0100

    Some completely untested prepare_release code.

--- doc/prepare_release.py
+++ doc/prepare_release.py
@@ -5,58 +5,184 @@ Copyright DeiC, deic.dk
 Licensed under the European Union Public License (EUPL) 1.2
 - see the LICENSE file for details.
 
-Automatically create the version switcher configuration for the PyData Sphinx Theme.
-See https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/version-dropdown.html#add-a-json-file-to-define-your-switcher-s-versions for details.
 
-The switcher will contain the latest 4 releases. This should be run before making a tag for the latest version.
-More information about releasing can be found here: https://cotainr.readthedocs.io/en/latest/development/releasing.html
+ This should be run before
+    making a tag for the latest version. More information about releasing can
+    befound here:
+    https://cotainr.readthedocs.io/en/latest/development/releasing.html
+
+
+
+
 """
 
+import argparse
+import datetime
 import json
 from pathlib import Path
-from re import match
+import re
 import subprocess
 import sys
 
-sys.path.insert(0, f'{(Path(__file__) / "../..").resolve()}')
+sys.path.insert(0, f"{Path('..').resolve()}")
 
 import cotainr
 
-tags = [
-    tag
-    for tag in (
-        subprocess.run(
-            ["git", "--no-pager", "tag"], capture_output=True, text=True
-        ).stdout.splitlines()
-    )
-    # Checking if the tag matches the versioning scheme YYYY.MM.MINOR
-    if match(r"^20[0-9]{2}\.(0[1-9]|10|11|12)\.[0-9]+$", tag) is not None
-]
-tags.reverse()
-
-switcher = [
-    {
-        "name": "dev",
-        "version": "latest",
-        "url": "https://cotainr.readthedocs.io/en/latest/",
-    },
-    {
-        "name": f"{cotainr.__version__} (stable)",
-        "version": "stable",
-        "url": "https://cotainr.readthedocs.io/en/stable/",
-    },
-]
-
-for tag in tags[0:3]:
-    switcher.append(
-        {
-            "name": tag,
-            "version": tag,
-            "url": f"https://cotainr.readthedocs.io/en/{tag}/",
-        }
-    )
-
-
-(Path(__file__) / "../_static/switcher.json").resolve().write_text(
-    json.dumps(switcher, indent="  ")
+COTAINR_RELEASE_VERSION_FORMAT_RE = (
+    r"^20[0-9]{2}\.(0[1-9]|10|11|12)\.[0-9]+$"  # cotainr YYYY.MM.MINOR version format
 )
+
+
+def create_docs_switcher(*, new_release_ver: str):
+    """
+    Create the version switcher for the documentation.
+
+    Automatically create the version switcher configuration for the PyData
+    Sphinx Theme. The switcher will contain the latest 4 releases. See
+    https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/version-dropdown.html#add-a-json-file-to-define-your-switcher-s-versions
+    for more technical details.
+
+    Parameters
+    ----------
+    new_release_ver : str
+        The YYYY.MM.MINOR version tag for the new release.
+    """
+    assert re.match(COTAINR_RELEASE_VERSION_FORMAT_RE, new_release_ver)
+
+    # Create base switcher with the latest and stable versions
+    switcher = [
+        {
+            "name": "dev",
+            "version": "latest",
+            "url": "https://cotainr.readthedocs.io/en/latest/",
+        },
+        {
+            "name": f"{new_release_ver} (stable)",
+            "version": "stable",
+            "url": "https://cotainr.readthedocs.io/en/stable/",
+        },
+    ]
+
+    # Append the latest 3 releases to the switcher based on git tags that match
+    # the cotainr version format
+    tags = [
+        tag
+        for tag in (
+            subprocess.run(
+                ["git", "--no-pager", "tag"], capture_output=True, text=True
+            ).stdout.splitlines()
+        )
+        if re.match(COTAINR_RELEASE_VERSION_FORMAT_RE, tag)
+    ]
+    tags.reverse()
+    for tag in tags[0:3]:
+        switcher.append(
+            {
+                "name": tag,
+                "version": tag,
+                "url": f"https://cotainr.readthedocs.io/en/{tag}/",
+            }
+        )
+
+    # Write the switcher to a static JSON file
+    version_switcher_file = (Path(__file__) / "../_static/switcher.json").resolve()
+    version_switcher_file.write_text(json.dumps(switcher, indent="  "))
+    print(
+        f"Version switcher written to {version_switcher_file}. "
+        "Please check its contents and commit it."
+    )
+
+
+def create_release_notes(*, new_release_ver: str, release_date: datetime.date):
+    """
+    Create the release notes for the new version.
+
+    Automatically create the skeleton for the release notes for the new
+    version. Fills in the version number in the release note template.
+
+    Parameters
+    ----------
+    new_release_ver : str
+        The YYYY.MM.MINOR version tag for the new release.
+    release_date : datetime.date
+        The release date for the new version.
+    """
+    assert re.match(COTAINR_RELEASE_VERSION_FORMAT_RE, new_release_ver)
+
+    release_notes = (
+        (
+            # Load release note template
+            (Path(__file__) / "../release_notes/release_note.md.template")
+            .resolve()
+            .read_text()
+        )
+        .replace(
+            # Insert the new version number as the title
+            "__YYYY.MM.MINOR__",
+            new_release_ver,
+        )
+        .replace(
+            # Insert the release date
+            "__Month__ __day__, __year__",
+            f"{_format_date(release_date)}",
+        )
+        .replace(
+            # Insert version tag for RTD link
+            "__version__tag__",
+            new_release_ver,
+        )
+    )
+
+    # Write the release note to a new file
+    release_notes_file = (
+        Path(__file__) / f"../release_notes/{new_release_ver}.md"
+    ).resolve()
+    release_notes_file.write_text(release_notes)
+    print(
+        f"Release notes skeleton written to {release_notes_file}. "
+        "Please fill in the release notes and commit it."
+    )
+
+
+def _format_date(date: datetime.date):
+    """
+    Format `date` as a "__Month__ __day__, __year__" string.
+
+    Parameters
+    ----------
+    date : datetime.date
+        The date to format.
+    """
+    # Determine english orginal prefix
+    if date.day in [1, 21, 31]:
+        day_prefix = "st"
+    elif date.day in [2, 22]:
+        day_prefix = "nd"
+    elif date.day in [3, 23]:
+        day_prefix = "rd"
+    else:
+        day_prefix = "th"
+
+    return date.strftime(f"%B %-d{day_prefix}, %Y")
+
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--release_date",
+        type=str,
+        help="The release date for the new version in YYYY.MM.DD format. The default is the current date.",
+    )
+    args = parser.parse_args()
+
+    if args.release_date is None:
+        release_date = datetime.datetime.today().date()
+    else:
+        release_date = datetime.datetime.strptime(args.release_date, "%Y.%m.%d").date()
+
+    new_release_ver = cotainr.__version__.split(".dev")[0]
+
+    # Create the version switcher for the documentation
+    create_docs_switcher(new_release_ver=new_release_ver)
+    create_release_notes(new_release_ver=new_release_ver, release_date=release_date)
