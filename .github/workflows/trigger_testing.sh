9f3862f171d74aa35ef18ea617445038a80250cf
JuliusRoeder <julius@roeder.dev>
2025-04-02 12:38:57 +0200

    move bash scripts into separate file

---
+++ .github/workflows/trigger_testing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: Bearer $GH_TOKEN" \
+  https://api.github.com/repos/$1/actions/workflows/CI_push.yml/dispatches \
+  -d '{"ref": "$2"}'
