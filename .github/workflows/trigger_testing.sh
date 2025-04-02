90113750840748dbb5a71ed57d2115a7629841c1
JuliusRoeder <julius@roeder.dev>
2025-04-02 13:45:20 +0200

    error

--- .github/workflows/trigger_testing.sh
+++ .github/workflows/trigger_testing.sh
@@ -4,4 +4,4 @@ curl -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: Bearer $GH_TOKEN" \
   https://api.github.com/repos/$1/actions/workflows/CI_push.yml/dispatches \
-  -d '{"ref": '"$2"'}'
+  -d '{"ref": "'"${2}"'"}'
