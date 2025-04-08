266ac6d5a5a0b1b9a73c3f7c030731d3496d24cc
Joachim Sødequist <139451906+joasode@users.noreply.github.com>
2025-04-08 11:07:48 +0200

    Update container.py

--- cotainr/container.py
+++ cotainr/container.py
@@ -64,6 +64,7 @@ class SingularitySandbox:
 
     def __init__(self, *, base_image, log_settings=None):
         """Construct the SingularitySandbox context manager."""
+        print('test')
         self.base_image = base_image
         self.sandbox_dir = None
         self.architecture = None
