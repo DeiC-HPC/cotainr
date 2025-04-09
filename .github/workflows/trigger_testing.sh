dcb46100c8a366373c01bf80915f8d08cb542652
JuliusRoeder <julius@roeder.dev>
2025-04-09 14:30:57 +0200

    Joachims simplification.

--- .github/workflows/trigger_testing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: Bearer $GH_TOKEN" \
-  https://api.github.com/repos/$1/actions/workflows/CI_push.yml/dispatches \
-  -d '{"ref": "'"${2}"'"}'
