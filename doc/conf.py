908d8c2e6eb0ab45e5cf1cbb423693b44da6c7bf
JuliusRoeder <julius@roeder.dev>
2025-05-01 13:29:56 +0200

    added gnu to the linkcheck_ignore

--- doc/conf.py
+++ doc/conf.py
@@ -57,6 +57,8 @@ linkcheck_ignore = [
     # We do have quite a lot of links to our own github repo
     # For now, let's avoid checking these
     "https://github.com/DeiC-HPC/cotainr",
+    # Also ignore everything on the gnu domain via regex.
+    r'https://.*\.gnu\.org/.*'
 ]
 linkcheck_anchors_ignore_for_url = [
     # Ignore GitHub issue comment anchors that apparently fails
